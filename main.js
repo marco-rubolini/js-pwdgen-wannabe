041bbe42cc01f1c401569e2a2f0b376d73c3785c
Marco <rubolini.marco@gmail.com>
2020-04-17 15:31:55 +0200

    Prompt e assegnazione Cognome utente

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 //Chiedere all'utente il suo nome
-var firstName = prompt ('Qual è il tuo nome');
+var firstName = prompt ('Qual è il tuo nome?');
 console.log(firstName);
 
 //Chiedere all'utente il suo cognome
-
+var lastName = prompt ('Qual è il tuo cognome?');
+console.log(lastName);
 //Chiedere all'utente il suo colore preferito
 
 //Generare la password utente
