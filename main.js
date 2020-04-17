d6f30f02a352cdc8e34301a0a63b4c9a53a9fd07
Marco <rubolini.marco@gmail.com>
2020-04-17 18:49:58 +0200

    Aggiunto commento file js

--- main.js
+++ main.js
@@ -5,11 +5,14 @@ console.log(firstName);
 //Chiedere all'utente il suo cognome
 var lastName = prompt ('Qual è il tuo cognome?');
 console.log(lastName);
+
 //Chiedere all'utente il suo colore preferito
 var favoriteColor = prompt('Qual è il tuo colore preferito?');
 console.log(favoriteColor);
+
 //Generare la password utente
 var yourPassword = firstName + lastName + favoriteColor + 20;
 console.log(yourPassword);
 
+//Scrivere la password generata
 document.getElementById('password').innerHTML = yourPassword
